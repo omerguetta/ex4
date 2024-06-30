6779ccd2700d87161aedcf45a10217b286db6a28
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-06-30 21:48:42 +0300

    create basic set index

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.set('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
+    res.set('Content-Type', 'application/json');
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
