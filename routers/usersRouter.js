45ac5d5dc7583cff145aa3c525756671c1c4d7db
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-07-01 00:14:39 +0300

    add userRouter.post

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -3,6 +3,7 @@ const { usersController } = require('../controllers/usersController');
 
 const userRouter = Router();
 
+userRouter.post('/register', usersController.register);
 
 
 module.exports = { userRouter };
