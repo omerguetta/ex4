2a3b741fd4cc797d396d1c661cc1f5a4649fc5c9
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-07-01 00:15:25 +0300

    add usercontrollers register

---
+++ controllers/usersController.js
@@ -0,0 +1,33 @@
+const { dbConnection } = require('../db_connections');
+const TABLE_PREFIX = 'tbl_50_users';
+
+exports.usersController = {
+    async register(req, res) {
+        const {username,password} = req.body;
+        if (!username || !password) {
+            res.status(400).send('username and password are required');
+            return;
+        }
+        const connection = await dbConnection.createConnection();
+        try {
+            const access_key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+            const [result] = await connection.execute(
+                `INSERT INTO ${TABLE_PREFIX} (user_name, password, access_key) VALUES (?, ?, ?)`,
+                [username, password,access_key]
+            );
+            if (result.affectedRows === 0) {
+                return res.status(500).json({ error: 'Internal server error at register user' });
+            }
+            res.status(201).json({
+                message: 'User registered successfully',
+                access_token: access_key
+            });
+        } catch (e) {
+            res.status(500).json({ error: 'Internal server error at register user =' });
+        } finally {
+            connection.end();
+        }
+
+    }
+
+}
