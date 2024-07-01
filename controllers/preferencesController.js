2553a8fac3ec707b61722fcefb8841e0d2a2cf1f
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-07-01 13:51:30 +0300

    create preferencesControllers and add getPreferences

---
+++ controllers/preferencesController.js
@@ -0,0 +1,21 @@
+    async getPreferenceByUserName(req,res){
+        const connection = await dbConnection.createConnection();
+        try {
+            const [result] = await connection.execute(
+                `SELECT user_id,start_date,end_date,vacation_destination,vacation_type
+                FROM tbl_50_preferences
+                INNER JOIN tbl_50_users ON tbl_50_preferences.user_id = tbl_50_users.id
+                WHERE tbl_50_users.user_name = ?`,
+                [req.params.username]
+            );
+            if (result.length === 0) {
+                return res.status(404).json({ error: 'Preferences not found' });
+            }
+            res.status(200).json(result);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        } finally {
+            await connection.end();
+        }
+    }
+
