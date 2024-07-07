371b77df9f322760ce2d6edeb8dc5ab5a32fb845
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-07-08 00:28:09 +0300

    fix the updatePreference

--- controllers/preferencesController.js
+++ controllers/preferencesController.js
@@ -146,30 +146,6 @@ exports.preferencesController = {
     async updatePreference(req,res){
         const {username} = req.params;
         const {start_date,end_date,vacation_destination,vacation_type,access_key} = req.body;
-        if (!start_date || !end_date || !vacation_destination || !vacation_type||!access_key) {
-            res.status(400).send('start_date, end_date, vacation_destination and vacation_type are required:');
-            return;
-        }
-        if (!(vacation_destinations.includes(vacation_destination))) {
-            res.status(400).send('vacation_destination is not valid:');
-            return;
-        }
-        if (!(vacation_types.includes(vacation_type))) {
-            res.status(400).send('vacation_type is not valid:');
-            return;
-        }
-        const startDate = new Date(start_date);
-        const endDate = new Date(end_date);
-        if (isNaN(startDate) || isNaN(endDate)) {
-            res.status(400).send('Invalid start_date or end_date.');
-            return;
-        }
-        const differenceInMilliseconds = endDate - startDate;
-        const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
-        if (differenceInDays > 7) {
-            res.status(400).send('vacation cannot be longer than a week');
-            return;
-        }
         const connection = await dbConnection.createConnection();
         try {
             const [result] = await connection.execute(
@@ -183,6 +159,30 @@ exports.preferencesController = {
                 return res.status(403).json({ error: 'Access key is not valid' });
             }
             const user_id = result[0].id;
+            if (!start_date || !end_date || !vacation_destination || !vacation_type||!access_key) {
+                res.status(400).send('start_date, end_date, vacation_destination and vacation_type are required:');
+                return;
+            }
+            if (!(vacation_destinations.includes(vacation_destination))) {
+                res.status(400).send('vacation_destination is not valid:');
+                return;
+            }
+            if (!(vacation_types.includes(vacation_type))) {
+                res.status(400).send('vacation_type is not valid:');
+                return;
+            }
+            const startDate = new Date(start_date);
+            const endDate = new Date(end_date);
+            if (isNaN(startDate) || isNaN(endDate)) {
+                res.status(400).send('Invalid start_date or end_date.');
+                return;
+            }
+            const differenceInMilliseconds = endDate - startDate;
+            const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+            if (differenceInDays > 7) {
+                res.status(400).send('vacation cannot be longer than a week');
+                return;
+            }
             const [updateResult] = await connection.execute(
                 `UPDATE ${TABLE_PREFIX} SET start_date = ?, end_date = ?, vacation_destination = ?, vacation_type = ? WHERE user_id = ?`,
                 [start_date, end_date, vacation_destination, vacation_type, user_id]
